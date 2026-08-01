f6a5b8ea04866172117bcdff3024f7e765bea750
Jason Morley <hello@jbmorley.co.uk>
2026-08-01 12:00:39 -1000

    ci: Use changes and build-tools from PyPI (#61)

--- scripts/build.sh
+++ scripts/build.sh
@@ -153,7 +153,8 @@ echo -n "$APPLE_API_KEY_BASE64" | base64 --decode -o "$API_KEY_PATH"
 build-tools notarize "$BUILD_DIRECTORY/TailLight.app" \
     --key "$API_KEY_PATH" \
     --key-id "$APPLE_API_KEY_ID" \
-    --issuer "$APPLE_API_KEY_ISSUER_ID"
+    --issuer "$APPLE_API_KEY_ISSUER_ID" \
+    --log-directory "$BUILD_DIRECTORY"
 
 # Compress the app.
 RELEASE_BASENAME="TailLight-$VERSION_NUMBER-$BUILD_NUMBER"
